40c07a7dca3131a3d9b4d5b27319a1f308832f49
foggynight <foggynight709@gmail.com>
2021-08-11 22:15:11 -0230

    Add tree traversal functions

--- src/general-tree.lisp
+++ src/general-tree.lisp
@@ -19,3 +19,38 @@ of its nodes, nor the number of children its nodes have."
       (cons (general-tree-data tree)
             (mapcar #'tree->sexp (general-tree-children tree)))
       tree))
+
+(defun inorder-traversal (tree)
+  "Perform an inorder traversal of a general tree and collect the data at each
+visited node in a list."
+  (if (general-tree-p tree)
+      (let* ((children (general-tree-children tree))
+             (center-index (ceiling (length children) 2)))
+        (append (reduce #'append
+                        (mapcar #'inorder-traversal
+                                (subseq children 0 center-index)))
+                (list (general-tree-data tree))
+                (reduce #'append
+                        (mapcar #'inorder-traversal
+                                (subseq children center-index)))))
+      (list tree)))
+
+(defun preorder-traversal (tree)
+  "Perform a preorder traversal of a general tree and collect the data at each
+visited node in a list."
+  (if (general-tree-p tree)
+      (cons (general-tree-data tree)
+            (reduce #'append
+                    (mapcar #'preorder-traversal
+                            (general-tree-children tree))))
+      (list tree)))
+
+(defun postorder-traversal (tree)
+  "Perform a postorder traversal of a general tree and collect the data at each
+visited node in a list."
+  (if (general-tree-p tree)
+      (append (reduce #'append
+                      (mapcar #'postorder-traversal
+                              (general-tree-children tree)))
+              (list (general-tree-data tree)))
+      (list tree)))
