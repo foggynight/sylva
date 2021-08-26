a14752444656a3856efac24f09f06ea1a29577b5
foggynight <foggynight709@gmail.com>
2021-08-26 00:09:50 -0230

    Add package exports

--- src/package.lisp
+++ src/package.lisp
@@ -2,4 +2,16 @@
   (:documentation "Library for various types of trees with a unified interface
 in Common Lisp.")
   (:use #:cl)
-  (:nicknames #:syl))
+  (:nicknames #:syl)
+  (:export
+
+   ;; general-tree.lisp
+   #:sexp->tree
+   #:tree->sexp
+   #:equal?
+   #:leaf?
+   #:preorder-traversal
+   #:postorder-traversal
+   #:inorder-traversal
+
+   ))
