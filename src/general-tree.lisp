df3f1a1762727596278cb138f757f0bbb654a203
foggynight <foggynight709@gmail.com>
2021-08-26 22:31:44 -0230

    Replace traversal functions with TRAVERSE...
    ...function and auxiliary methods.
    
    Also updated docstrings to refer to the function parameters directly,
    i.e. "TREE" instead of "a tree".

--- src/general-tree.lisp
+++ src/general-tree.lisp
@@ -26,14 +26,14 @@ TREE-TYPE parameter."
   (%sexp->tree sexp tree-type))
 
 (defun tree->sexp (tree)
-  "Convert a tree into a sexp."
+  "Convert TREE into a sexp."
   (if (general-tree-p tree)
       (cons (general-tree-data tree)
             (mapcar #'tree->sexp (general-tree-children tree)))
       tree))
 
 (defun leaf? (tree)
-  "Is a tree a leaf?"
+  "Is TREE a leaf node?"
   (endp (general-tree-children tree)))
 
 (defun equal? (tree-0 tree-1)
@@ -41,37 +41,55 @@ TREE-TYPE parameter."
   (tree-equal (tree->sexp tree-0)
               (tree->sexp tree-1)))
 
-(defun preorder-traversal (tree)
-  "Perform a preorder traversal on a tree and collect the data at each visited
-node in a list."
+(defmethod %traverse (tree (order (eql 'preorder)) function)
   (if (general-tree-p tree)
-      (cons (general-tree-data tree)
+      (cons (if function
+                (funcall function (general-tree-data tree))
+                (general-tree-data tree))
             (reduce #'append
-                    (mapcar #'preorder-traversal
+                    (mapcar (lambda (e) (preorder-traversal e function))
                             (general-tree-children tree))))
-      (list tree)))
+      (list (if function
+                (funcall function tree)
+                tree))))
 
-(defun postorder-traversal (tree)
-  "Perform a postorder traversal on a tree and collect the data at each visited
-node in a list."
+(defmethod %traverse (tree (order (eql 'postorder)) function)
   (if (general-tree-p tree)
       (append (reduce #'append
-                      (mapcar #'postorder-traversal
+                      (mapcar (lambda (e) (postorder-traversal e function))
                               (general-tree-children tree)))
-              (list (general-tree-data tree)))
-      (list tree)))
+              (list (if function
+                        (funcall function (general-tree-data tree))
+                        (general-tree-data tree))))
+      (list (if function
+                (funcall function tree)
+                tree))))
 
-(defun inorder-traversal (tree)
-  "Perform an inorder traversal on a tree and collect the data at each visited
-node in a list."
+(defmethod %traverse (tree (order (eql 'inorder)) function)
   (if (general-tree-p tree)
       (let* ((children (general-tree-children tree))
              (center-index (ceiling (length children) 2)))
         (append (reduce #'append
-                        (mapcar #'inorder-traversal
+                        (mapcar (lambda (e) (inorder-traversal e function))
                                 (subseq children 0 center-index)))
-                (list (general-tree-data tree))
+                (list (if function
+                          (funcall function (general-tree-data tree))
+                          (general-tree-data tree)))
                 (reduce #'append
-                        (mapcar #'inorder-traversal
+                        (mapcar (lambda (e) (inorder-traversal e function))
                                 (subseq children center-index)))))
-      (list tree)))
+      (list (if function
+                (funcall function tree)
+                tree))))
+
+(defun traverse (tree &key (order 'preorder) function)
+  "Traverse a tree and collect the data at each visited node in a list.
+
+The order of the traversal may be specified by passing a symbol naming the
+traversal order to the ORDER key parameter.
+
+A function may be passed to the FUNCTION key parameter, it will be called on
+each of the nodes in the tree by passing the data point of the node as the only
+argument to FUNCTION, its result taking the place of the node's data point in
+the list returned from this function."
+  (%traverse tree order function))
